ff14d704206cedc822ffb790967966b9d51215fe
DeucesDevops <macroni607@gmail.com>
2024-01-26 23:32:12 +0000

    added product images and rendered new list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-
+import { sampleProducts } from "./data"
 
 function App() {
  
@@ -10,7 +10,14 @@ function App() {
       </header>
 
       <main>
-
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className="product-image" />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+        ))}</ul>
       </main>
 
       <footer>
