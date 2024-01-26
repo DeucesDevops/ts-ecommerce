60a4a777b05a0af15441e8106a752efa5de3bad8
DeucesDevops <macroni607@gmail.com>
2024-01-26 22:31:31 +0000

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <header>
+        TS ShopX
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+        All rights reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
